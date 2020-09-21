748b11094c564865ca26c7db43ecf1de25cd8771
J K <kj688895@gmail.com>
2020-09-21 22:18:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,8 +32,7 @@ class TicTacToe
 
   def position_taken?(index)
     @board[index] == "X" || @board[index] == "O"
-    end
-  end
+  end 
 
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
