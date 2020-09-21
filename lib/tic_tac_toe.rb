1fc6ce0d6269eb176667f4f02029d033e327e7a1
J K <kj688895@gmail.com>
2020-09-21 22:10:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,11 +31,11 @@ class TicTacToe
 
   def position_taken?(index)
     if @board[index] == "X" || @board[index] == "O"
-    end 
+    end
   end
 
   def valid_move?(index)
-    !position_taken?(index) && @board[index]
+    index.between?(0,8) && !position_taken?(index)
   end
 
   def turn_count
