b0f4c854286b6684ec4adebb2a8a574bfc62d258
J K <kj688895@gmail.com>
2020-09-21 21:30:51 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,76 @@
+class TicTacToe
+  def initialize
+    @board = Array.new(9, " ")
+  end
+
+  WIN_COMBINATIONS =
+  [[0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [6,4,2],
+  [0,4,8],]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(string)
+    @choose = string.to_i-1
+  end
+
+  def move(input_to_index, person)
+    @board [input_to_index] = person
+  end
+
+  def position_taken?(input_to_index)
+    if @board[input_to_index] == "X" || @board[input_to_index] == "O"
+      true
+    else
+      false
+    end
+  end
+
+  def valid_move?(input_to_index)
+    !position_taken?(input_to_index) && @board[input_to_index]
+  end
+
+  def turn_count
+    count = 0
+    @board.each do |i|
+      count += 1 if i == "X" || i == "O"
+    end
+    count
+  end
+
+  def current_player
+    person = nil
+    if turn_count() % 2 == 0
+      person = "X"
+    else
+      person = "O"
+    end
+    person
+  end
+
+  def turn
+    puts "Enter 1-9"
+    user_input = gets.strip
+    user_input = input_to_index(user_input)
+    if valid_move?(user_input)
+      move(user_input, current_player)
+    else
+      turn
+    end
+    display_board
+  end
+
+
+
+end
