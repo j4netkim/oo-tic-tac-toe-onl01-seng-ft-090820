9b32adef126e2034838695c113e55dd54a399e5b
J K <kj688895@gmail.com>
2020-09-21 22:12:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,12 +61,12 @@ class TicTacToe
 
 
   def won?
-    WIN_COMBINATIONS.detect do |combo|
-      @board[combo[0]] == @board[combo[1]] &&
-      @board[combo[1]] == @board[combo[2]] &&
-      position_taken?(combo[0])
+    WIN_COMBINATIONS.any? do |combo|
+    if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] == @board[combo[2]]
+      return combo
     end
   end
+end 
 
   def full?
     @board.all?{|token| token == "X" || token == "O"}
